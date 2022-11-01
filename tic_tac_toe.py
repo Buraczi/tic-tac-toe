f2969c381b97d88e654380f660d249ae5ccd0071
unknown <oprestgog@gmail.com>
2022-11-01 11:50:42 +0100

    add new class

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,2 +1,10 @@
-"""A tic-tac-toe game built with Python and Tkinter."""
+#tic_tac_toe.py
 
+import tkinter as tk
+from tkinter import font
+
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic-Tac-Toe Game")
+        self._cells = {}
